b2191226191f49c2520b51ce1cfb4c194470064f
joelkay <joeldkay01@gmail.com>
2017-03-14 12:55:37 +0000

    Update main.js
    add fullscreen modal

--- main.js
+++ main.js
@@ -19,6 +19,16 @@
 
     return false;
   });
+  
+  $(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+  });
+  
+  $(".modal-fullscreen").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  });
 
  
 })(jQuery);
